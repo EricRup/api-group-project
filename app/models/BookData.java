b55c5d03bc6c0ba9e73d19acb3e753f2104a8116
mqy5131 <mqy5131@users.noreply.github.com>
2017-08-09 14:00:59 -0400

    Update to BookData - added one more Book instance.

--- app/models/BookData.java
+++ app/models/BookData.java
@@ -9,6 +9,7 @@ public class BookData{
 		books.put(1, new Book(9781785882562L, "Learning Network Programming with Java", "R. M. Reese", 1, 2015));
 		books.put(2, new Book(9780134177335L, "Core Java Volume 1 - Fundamentals", "C. S. Hostmann", 10, 2015));
 		books.put(3, new Book(978178439637L, "RESTful Java Web Services", "J. Purushothaman", 2, 2015));
+		books.put(4, new Book(9781617290909L, "Play for Java: Covers Play 2", "N. Leroux", 3, 2014));
 	}
 	
 	public Book getBook(int id){
